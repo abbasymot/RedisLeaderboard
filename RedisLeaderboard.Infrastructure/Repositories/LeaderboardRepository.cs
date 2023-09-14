70709e3c94619a94cb9970c2f0377669f988f679
abbasymot <motameditalemi@gmail.com>
2023-09-14 13:39:46 +0330

    inject the redis database in LeaderboardRepository

--- RedisLeaderboard.Infrastructure/Repositories/LeaderboardRepository.cs
+++ RedisLeaderboard.Infrastructure/Repositories/LeaderboardRepository.cs
@@ -1,10 +1,22 @@
 using RedisLeaderboard.Domain.Models;
 using RedisLeaderboard.Domain.Repositories;
+using StackExchange.Redis;
 
 namespace RedisLeaderboard.Infrastructure.Repositories;
 
 public class LeaderboardRepository : ILeaderboardRepository
 {
+    #region Dependency Injection
+
+    private readonly IDatabase _db;
+
+    public LeaderboardRepository(RedisContext redisContext)
+    {
+        this._db = redisContext.Database;
+    }
+
+    #endregion
+
     public Task AddScore(string username, string stat, decimal score)
     {
         throw new NotImplementedException();
